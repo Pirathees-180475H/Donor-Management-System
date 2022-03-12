3c267e7aa7f6d3c34c085905b48eaab27e61c4ac
Pirathees <kamalanathanpirathees@gmail.com>
2022-03-12 07:02:05 +0530

    extended Servelet Initalizer

--- src/main/java/com/DMS/DmsMicro/DmsMicroApplication.java
+++ src/main/java/com/DMS/DmsMicro/DmsMicroApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DmsMicroApplication {
-
+public class DmsMicroApplication extends  ServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(DmsMicroApplication.class, args);
 	}
