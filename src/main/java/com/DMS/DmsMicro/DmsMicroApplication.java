9e94f07f3937088b16a6a1f3430593de8936c1b5
Pirathees <kamalanathanpirathees@gmail.com>
2022-03-12 08:12:25 +0530

    last try

--- src/main/java/com/DMS/DmsMicro/DmsMicroApplication.java
+++ src/main/java/com/DMS/DmsMicro/DmsMicroApplication.java
@@ -2,11 +2,17 @@ package com.DMS.DmsMicro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DmsMicroApplication extends  ServletInitializer {
+public class DmsMicroApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(DmsMicroApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DmsMicroApplication.class);
+	}
 }
